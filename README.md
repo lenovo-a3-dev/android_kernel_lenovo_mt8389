47c10a02b86c3decea9d4075c5c2db16cdec877b
kupyxa4444 <kupyxa4444@gmail.com>
2014-10-30 00:51:17 +0500

    Update README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+sorce code for LENOVO A3000 and S6000
+for building kernel, please use "kernel.sh", changing for your`s paths and config
--- pack_bootimage.sh
+++ pack_bootimage.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-#make to bootimage.sh
-if [ -z $1 ] 
-then
-  echo 'usage: "./pack_bootimage.sh  projectname"'
-else
- if [ "a3000_prc_3gfull" == $1 ]; then
-  PRJ=$1
- elif [ "13019" == $1 ]; then
-  PRJ=OPPO89_$1
- fi
- out/host/linux-x86/bin/acp -uv kernel/out/kernel_${PRJ}.bin out/target/product/${PRJ}/kernel
- out/host/linux-x86/bin/mkbootfs out/target/product/${PRJ}/root | out/host/linux-x86/bin/minigzip > out/target/product/${PRJ}/ramdisk.img
- mediatek/build/tools/mkimage out/target/product/${PRJ}/ramdisk.img ROOTFS > out/target/product/${PRJ}/ramdisk_android.img
- mv out/target/product/${PRJ}/ramdisk.img mediatek/kernel/trace32/${PRJ}_ramdisk.img
- mv out/target/product/${PRJ}/ramdisk_android.img out/target/product/${PRJ}/ramdisk.img
- out/host/linux-x86/bin/mkbootimg  --kernel out/target/product/${PRJ}/kernel --ramdisk out/target/product/${PRJ}/ramdisk.img --board 1336460062 --output out/target/product/${PRJ}/boot.img
- echo "pack Bootimage done!"
-fi
+
+set -e
+
+KERNEL_DIR=/home/olivier/a3000_kernel_jb
+export TARGET_PRODUCT=a3000_row_call
+RAMDISK_DIRECTORY=/home/olivier/a3-kernel/ramdisk/$TARGET_PRODUCT
+
+rm -rf $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/kernel
+rm -rf $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/boot.img
+$KERNEL_DIR/mediatek/build/tools/mkimage $KERNEL_DIR/kernel/out/arch/arm/boot/zImage KERNEL > $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/kernel
+
+/home/olivier/a3-kernel/mtk-tools/repack-MTK.pl -boot $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/kernel $RAMDISK_DIRECTORY $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/boot.img
