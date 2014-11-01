166597728e6a0845d6d39336d25d3998e551d7b6
olivieer <yves.olli@gmail.com>
2014-11-01 14:27:22 -0400

    Cleanup
    Remove everything that's not necessarily required

--- bootable/bootloader/lk/Android.mk
+++
@@ -1,1 +0,0 @@
-# barrier for searching Android.mk
--- external/kernel-headers/original/linux/autoconf.h
+++
@@ -1,8 +0,0 @@
-#ifndef LINUX_AUTOCONF_CRAP_GOES_HERE
-#define LINUX_AUTOCONF_CRAP_GOES_HERE
-
-/* we're not using this to build kernel stuff, so a subset should do */
-
-#define AUTOCONF_INCLUDED
-
-#endif
