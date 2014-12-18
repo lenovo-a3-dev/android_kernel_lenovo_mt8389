87c309178399304414abc255d2bded6f26428f23
olivieer <yves.olli@gmail.com>
2014-12-18 08:20:06 -0500

    a3: Update partition table
    Change-Id: I54f57dadf8549b28aef82a9fa8268b493b15350e

--- mediatek/platform/mt6589/kernel/drivers/dum-char/partition_define.c
+++ mediatek/platform/mt6589/kernel/drivers/dum-char/partition_define.c
@@ -52,10 +52,10 @@ struct excel_info PartInfo[PART_NUM]={
 			{"logo",3145728,0x3a00000, EMMC, 0,USER},
 			{"ebr2",524288,0x3d00000, EMMC, 0,USER},
 			{"expdb",10485760,0x3d80000, EMMC, 0,USER},
-			{"android",681574400,0x4780000, EMMC, 5,USER},
-			{"cache",132120576,0x2d180000, EMMC, 6,USER},
-			{"usrdata",1073741824,0x34f80000, EMMC, 7,USER},
-			{"fat",0,0x74f80000, EMMC, 8,USER},
+			{"fac",209715200,0x4780000, EMMC, 5,USER},
+			{"android",838860800,0x10f80000, EMMC, 6,USER},
+			{"cache",132120576,0x42f80000, EMMC, 7,USER},
+			{"usrdata",1073741824,0x4ad80000, EMMC, 8,USER},
 			{"bmtpool",22020096,0xFFFF00a8, EMMC, 0,USER},
  };
 EXPORT_SYMBOL(PartInfo);
