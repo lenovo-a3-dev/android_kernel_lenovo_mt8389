b03b00daa0b014f43ff886fbafdb7cb07ea7799a
olivieer <yves.olli@gmail.com>
2014-12-29 07:47:03 -0500

    Cleanup
    Change-Id: I34cf34b01ea1b017d64075bb36dffc6f338d4426

--- mediatek/custom/out/a3000_prc_3gfull/cgen/inc/custom_cfg_module_default.h
+++
@@ -1,5 +0,0 @@
-#include "../cfgdefault/CFG_WIFI_Default.h"
-#include "../cfgdefault/CFG_Custom1_Default.h"
-#include "../cfgdefault/CFG_GPS_Default.h"
-#include "../cfgdefault/CFG_PRODUCT_INFO_Default.h"
-#include "../cfgdefault/CFG_CustomSN_Default.h"
--- mediatek/custom/out/a3000_prc_3gfull/factory/inc/cust_led.h
+++
@@ -1,7 +0,0 @@
-#ifndef FTM_CUST_LED_H
-#define FTM_CUST_LED_H
-
-#define CUST_LED_HAVE_RED
-#define CUST_LED_HAVE_GREEN
-
-#endif /* FTM_CUST_LED_H */
