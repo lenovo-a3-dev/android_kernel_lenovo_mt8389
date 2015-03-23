813c18fa882adcfc4ff16941c45ee83d36885a07
olivieer <yves.olli@gmail.com>
2015-03-23 21:04:48 +0100

    treewide: Massive cleanup
    Change-Id: Ie7055be6949f2bf861639cf8c95b927bf435ffbf

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 14 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 SMP PREEMPT Sat Nov 29 19:02:55 EST 2014"
-#define LINUX_COMPILE_BY "olivier"
-#define LINUX_COMPILE_HOST "server1.teamblueridge.org"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,3 +0,0 @@
-#define UTS_RELEASE "3.4.5"
-#define BUILD_INFO "ALPS.JB2.MP.V1"
-#define BUILD_FINGERPRINT "a3000_row_call"
